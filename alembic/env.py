5b39efd8d99004d26e7882e9ec014b40a99be04d
jaman-bala <ermekoffdastan@yandex.ru>
2024-07-22 10:45:54 +0600

    up:15

--- alembic/env.py
+++ alembic/env.py
@@ -77,8 +77,10 @@
 
 import os
 from logging.config import fileConfig
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import create_engine
+from sqlalchemy import pool
 from alembic import context
+from sqlmodel import SQLModel
 
 from backend.src.account.user.models import BaseUser
 from backend.src.regions.models import BaseRegion
@@ -97,7 +99,6 @@ target_metadata = (
 
 url = os.environ.get("SQLALCHEMY_DATABASE_URL", config.get_main_option("sqlalchemy.url"))
 
-
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode."""
     context.configure(
@@ -110,10 +111,9 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode."""
-    connectable = create_async_engine(url, echo=True, future=True)
+    connectable = create_engine(url, echo=True, future=True, poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
         context.configure(
@@ -123,8 +123,8 @@ def run_migrations_online() -> None:
         with context.begin_transaction():
             context.run_migrations()
 
-
 if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
+
